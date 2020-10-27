6bf4b84bc064be3e0c77460ac021106db350f508
Fangheng Sun <fangheng.sun@huilianyi.com>
2020-10-27 16:22:13 +0800

    创建项目

---
+++ src/main/java/com/fangheng/meepo/MeepoApplication.java
@@ -0,0 +1,13 @@
+package com.fangheng.meepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fangheng/meepo/MeepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fangheng.meepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
